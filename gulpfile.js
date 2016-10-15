94ca92d059a18bac313d0f1e65e8b300755b6acd
Mike <michaelschultz01@gmail.com>
2016-10-15 14:46:33 -0700

    Adds landing page for github pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const appStyle = {
+  source: './app/styles/**/*.scss',
+  dest: './app/styles'
+};
+const ghPageStyle = {
+  source:  './assets/**/*.scss',
+  dest:  './assets'
+};
 
-gulp.task('sass', () => {
-  return gulp.src('./app/styles/**/*.scss')
+gulp.task('sass:app', () => { return compileSCSS(appStyle) } );
+gulp.task('sass:gh', () => { return compileSCSS(ghPageStyle) } );
+
+gulp.task('sass:app:watch', () => {
+  gulp.watch(appStyle.source, ['sass']);
+});
+
+gulp.task('sass:gh:watch', () => {
+  gulp.watch(ghPageStyle.source, ['sass']);
+});
+
+function compileSCSS(path) {
+  return gulp.src(path.source)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions']
+      browsers: ['> 5%']
     }))
-    .pipe(gulp.dest('./app/styles'));
-});
-
-gulp.task('sass:watch', () => {
-  gulp.watch('./app/styles/**/*.scss', ['sass']);
-});
+    .pipe(gulp.dest(path.dest));
+}
