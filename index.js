6dd6838d80d1b29474fbe755c9d370ff76cb2bb9
Amit Merchant <bullredeyes@gmail.com>
2016-10-20 06:40:38 +0530

    Add a context menu on the app icon on right click for Windows (#16)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const electron = require('electron');
 const menubar = require('menubar')({
   width: 370,
   height: 410,
@@ -7,11 +8,43 @@ const menubar = require('menubar')({
   tooltip: "Materialette",
   resizable: false
 });
+
+const contextMenu = electron.Menu.buildFromTemplate([
+  {
+    label: 'About',
+    click() {
+      electron.dialog.showMessageBox({title: "Materialette", type:"info", message: "Material Color Palette for macOS, Windows, and Linux", buttons: ["Close"] });
+    }
+  },
+  {
+    label: 'Website',
+    click() {
+      electron.shell.openExternal("https://github.com/mike-schultz/materialette");
+    }
+  },
+  {
+    type: 'separator'
+  },
+  {
+    label: 'Quit',
+    click() {
+      menubar.app.quit();
+    }
+  }
+
+]);
+
 menubar.on('ready', ()=>{
   global.sharedObj = {
     hide: menubar.hideWindow,
     quit: menubar.app.quit,
     pinned: false
+  }
+  
+  // This will add a context menu on the 
+  // app icon on right click for Windows
+  if (process.platform == 'win32') {
+    menubar.tray.setContextMenu(contextMenu);
   }
 });
 
